77d97f87337c79a22c4045ea6d3428c9e3d9ffce
Jacob Oyebanji <oyebanji05@gmail.com>
2015-12-08 20:47:29 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,91 @@
+
+
+var express = require("express");
+var app = express();
+var bodyParser =  require("body-parser");
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/testDB'); // connect to our database
+
+var Bear     = require('./app/models/bear');
+
+
+var port = process.env.PORT || 8001;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+
+
+var router = express.Router();
+
+router.get('/', function(req,res){
+	res.json({ message: 'hooray! welcome to our api!' });   
+})
+
+
+router.route('/bears')
+	.post(function(req,res){
+		var bear = new Bear();      // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+	})
+	.get(function(req,res){
+		
+		Bear.find(function(err,bears){
+			if(err)
+				res.send(err);
+			res.send(bears);
+		})
+	});
+	
+router.route('/bears/:bear_id')
+	.get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+	.put(function(req,res){
+		Bear.findById(req.params.bear_id,function(err,bear){
+			if(err)
+				res.send(err);
+			bear.name = req.body.name;
+			bear.save(function(err){
+				if(err)
+					res.send(err)
+				res.json({ message: "bear Updated !!!"});
+			})
+		});
+	})
+	.delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+// middleware to use for all requests
+app.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.use('/api',router);
+
+
+app.listen(port);
+
+console.log("Hurrrray");
+
